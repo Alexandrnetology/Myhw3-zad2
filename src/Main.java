eab73840bb971d60eca3bf8e6ac4b6a9dcd9896f
alexander test <martunov2013@mail.ru>
2024-02-25 12:08:13 +0300

    Added programm

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        BmiService service = new BmiService();
+        double height= 1.87; // "m"
+        int weight = 98; // "kg"
+        int index = service.calculate(1.87, 98);
+        System.out.println("Bmiindex =" + index);
+    }
 }
